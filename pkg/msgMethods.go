dd55b7996e913780a0410f23a8e3b50ece438be7
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-09-01 15:26:57 +0500

    feat: create editMsgMessage method

--- pkg/msgMethods.go
+++ pkg/msgMethods.go
@@ -4,15 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"os"
 )
 
 const (
-	sendMessageMethod     = "sendMessage"
-	deleteMessageMethod   = "deleteMessage"
-	editMediaMethod       = "editMessageMedia"
-	editReplyMarkupMethod = "editMessageReplyMarkup"
-	editTextMethod        = "editMessageText"
+	sendMessageMethod   = "sendMessage"
+	deleteMessageMethod = "deleteMessage"
 )
 
 func (c *Client) SendMessage(
@@ -70,100 +66,6 @@ func (c *Client) DeleteMessage(
 		body,
 		getUpdatesMethod,
 	); err != nil {
-		return nil, err
-	}
-
-	return response, nil
-}
-
-func (c *Client) EditMessageMedia(
-	chatId int64,
-	messageId int64,
-	mediaType string,
-	media os.File,
-	markup *InlineMarkup,
-) (*MessageResponse, error) {
-	params := map[string]string{
-		"chat_id":    fmt.Sprintf("%d", chatId),
-		"message_id": fmt.Sprintf("%d", messageId),
-		"media": fmt.Sprintf(`{
-			"type": "%s",
-			"media": "attach://%s"
-		}`, mediaType, mediaType),
-	}
-	if markup != nil {
-		jsonMarkup, err := json.Marshal(markup)
-		if err != nil {
-			log.Println("Ошибка преобразования markup json: ", err)
-			return nil, err
-		}
-		params["reply_markup"] = string(jsonMarkup)
-	}
-
-	response, err := c.mediaRequest(
-		params,
-		editMediaMethod,
-		map[string]os.File{mediaType: media},
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	return response, nil
-}
-
-func (c *Client) EditMessageReplyMarkup(
-	chatId int64,
-	messageId int64,
-	markup InlineMarkup,
-) (*MessageResponse, error) {
-	jsonMarkup, err := json.Marshal(markup)
-	if err != nil {
-		log.Println("Ошибка преобразования markup json: ", err)
-		return nil, err
-	}
-	params := map[string]string{
-		"chat_id":      fmt.Sprintf("%d", chatId),
-		"message_id":   fmt.Sprintf("%d", messageId),
-		"reply_markup": string(jsonMarkup),
-	}
-
-	response, err := c.messageRequest(
-		params,
-		editReplyMarkupMethod,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	return response, nil
-}
-
-func (c *Client) EditMessageText(
-	chatId int64,
-	messageId int64,
-	text string,
-	markup *InlineMarkup,
-) (*MessageResponse, error) {
-	params := map[string]string{
-		"message_id": fmt.Sprintf("%d", messageId),
-		"chat_id":    fmt.Sprintf("%d", chatId),
-		"text":       text,
-	}
-	if markup != nil {
-		jsonMarkup, err := json.Marshal(markup)
-		if err != nil {
-			log.Println("Ошибка преобразования markup json: ", err)
-			return nil, err
-		}
-		params["reply_markup"] = string(jsonMarkup)
-	}
-
-	response, err := c.messageRequest(
-		params,
-		editTextMethod,
-	)
-	if err != nil {
 		return nil, err
 	}
 
